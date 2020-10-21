97242a0212626e4f3ea60eebd4468442f5c7e0f5
SallyMostafa <47418938+SallyMostafa@users.noreply.github.com>
2020-10-21 16:50:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,58 @@
-from flask import Flask
+import logging
+import os
+import google.cloud.storage as storage
+from flask import Flask, request
 app = Flask(__name__)
 
+# Configure this environment variable via app.yaml
+GCS_IMAGE_BUCKET = os.environ['GCS_IMAGE_BUCKET']
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return """
+<form method="POST" action="/upload" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <input type="submit">
+</form>
+"""
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    """Process the uploaded file and upload it to Google Cloud Storage."""
+    uploaded_file = request.files.get('file')
+
+    if not uploaded_file:
+        return 'No file uploaded.', 400
+
+    # Create a Cloud Storage client.
+    gcs = storage.Client()
+
+    # Get the bucket that the file will be uploaded to.
+    bucket = gcs.get_bucket(GCS_IMAGE_BUCKET)
+
+    # Create a new blob and upload the file's content.
+    blob = bucket.blob(uploaded_file.filename)
+
+    blob.upload_from_string(
+        uploaded_file.read(),
+        content_type=uploaded_file.content_type
+    )
+
+    # The public URL can be used to directly access the uploaded file via HTTP.
+    return blob.public_url
+
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
