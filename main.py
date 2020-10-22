bfaed99df24cbb383309cbf66b23836768d6297a
SallyMostafa <47418938+SallyMostafa@users.noreply.github.com>
2020-10-21 20:46:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import logging
 import os
 import google.cloud.storage as storage
-from flask import Flask, request
+from google.cloud import datastore
+from flask import Flask, request, render_template
+import datetime
+
 app = Flask(__name__)
 
 # Configure this environment variable via app.yaml
 GCS_IMAGE_BUCKET = os.environ['GCS_IMAGE_BUCKET']
+datastore_client = datastore.Client()
 
 @app.route('/')
 def index():
-    return """
-<form method="POST" action="/upload" enctype="multipart/form-data">
-    <input type="file" name="file">
-    <input type="submit">
-</form>
-"""
+    # Fetch the most recent 10 access times from Datastore.
+    images = fetch_images(10)
 
+    return render_template(
+        'index.html', images=images)
+
+#     return """
+# <form method="POST" action="/upload" enctype="multipart/form-data">
+#     <input type="file" name="file">
+#     <input type="submit">
+# </form>
+# """
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -38,10 +47,31 @@ def upload():
         uploaded_file.read(),
         content_type=uploaded_file.content_type
     )
-
+    store_image_data(blob.public_url, 'photographer1', 'location1', datetime.datetime(2018, 1, 1, 10, 0, 0))
     # The public URL can be used to directly access the uploaded file via HTTP.
-    return blob.public_url
+    #return blob.public_url
+    return render_template(
+        'index.html', images=fetch_images(10))
 
+
+
+def store_image_data(url, photographer,location, date):
+    entity = datastore.Entity(key=datastore_client.key('image'))
+    entity.update({
+        'url': url,
+        'photographer': photographer,
+        'location': location,
+        'date': date
+    })
+    datastore_client.put(entity)
+
+
+def fetch_images(limit):
+    query = datastore_client.query(kind='image')
+    query.order = ['-url']
+
+    images = query.fetch(limit=limit)
+    return images
 
 @app.errorhandler(500)
 def server_error(e):
